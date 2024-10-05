dffd7a1917f28d2c7ca9902205c981da514b4d36
Jessicakgs <jessicakauane20@gmail.com>
2024-10-05 11:38:42 -0300

    First Commit

---
+++ src/test/java/manage/book/catalog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package manage.book.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
